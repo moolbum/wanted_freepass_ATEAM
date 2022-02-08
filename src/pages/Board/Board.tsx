ffdcac033560c445ceb8b2503fe4e17cf9f8c8d7
yangddu <wndun21@naver.com>
2022-02-09 04:18:23 +0900

    Pull

--- src/pages/Board/Board.tsx
+++ src/pages/Board/Board.tsx
@@ -25,7 +25,6 @@ const Board = () => {
   console.log(filterInfo);
 
   return (
-<<<<<<< HEAD
     <S.Wrapper>
       <S.Title>들어온 요청</S.Title>
       <S.Subtitle>파트너님에게 딱 맞는 요청서를 찾아보세요</S.Subtitle>
@@ -35,18 +34,10 @@ const Board = () => {
         ))}
         <S.Button>필터링 리셋</S.Button>
       </S.FilterContainer>
-=======
-    <div>
-      <S.TitleWrap>
-        <S.TitleSubWrap>
-          <div>가공방식, 재료</div>
-          <div>
-            <Toggle />
-            상담 중인 요청만 보기
-          </div>
-        </S.TitleSubWrap>
-      </S.TitleWrap>
->>>>>>> main
+      <div>
+        <Toggle />
+        상담 중인 요청만 보기
+      </div>
       <S.CardWrap>
         {cardInfo.map(function (requests, index) {
           return <Card key={index} {...requests} />;
